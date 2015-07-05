e99b01add07e13c13070532b817b79d11f5dfca1
alexandrgureev <agureev@lohika.com>
2015-07-05 19:10:04 +0300

    initialize bower and basic class

---
+++ rql.js
@@ -0,0 +1,19 @@
+
+var RQL = RQL || {};
+
+RQL.subns = (function() {
+    var internalState = "Message";
+
+    var privateMethod = function() {
+        return internalState;
+    };
+
+    var publicMethod = function() {
+        return privateMethod() + " stuff";
+    };
+
+    return {
+        someProperty: 'prop value',
+        publicMethod: publicMethod
+    };
+}) ();
