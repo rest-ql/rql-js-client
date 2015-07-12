836016f8a87014193ceec04030d0ac9ebf7bc325
alexandrgureev <agureev@lohika.com>
2015-07-12 19:57:01 +0300

    fix error in call

--- src/client/http_client.js
+++ src/client/http_client.js
@@ -3,55 +3,53 @@
 
     var Q = require("../../vendor/q");
 
-    function HttpClient () {
-        function send () {
-            var req = new XMLHttpRequest();
-            var deferred = Q.defer();
+    function send () {
+        var req = new XMLHttpRequest();
+        var deferred = Q.defer();
 
-            req.open('POST', "http://local.vm/api", true);
-            //req.open("POST","ajax_test.asp",true);
-            //req.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-            req.setRequestHeader("Content-type","application/json");
+        req.open('POST', "http://local.vm/api", true);
+        //req.open("POST","ajax_test.asp",true);
+        //req.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+        req.setRequestHeader("Content-type","application/json");
 
-            req.onload = function (e) {
-                if (req.readyState === 4) {
-                    //var headers = req.getAllResponseHeaders().toLowerCase();
-                    //console.log(headers, req.responseText, req.status );
-                    //console.log(JSON.parse(req.responseText));
-                    //console.log(req.responseText);
-                    deferred.resolve(req.responseText);
-                } else {
-                    deferred.reject(req.status)
-                }
-            };
-
-            req.onerror = function (e) {
-                console.error(req.statusText);
-            };
-
-            req.send('{"a": "safasdf"}');
-            //req.send(null);
-            //req.send("fname=Henry&lname=Ford");
-
-            return deferred.promise;
+        req.onload = function (e) {
+            if (req.readyState === 4) {
+                //var headers = req.getAllResponseHeaders().toLowerCase();
+                //console.log(headers, req.responseText, req.status );
+                //console.log(JSON.parse(req.responseText));
+                //console.log(req.responseText);
+                deferred.resolve(req.responseText);
+            } else {
+                deferred.reject(req.status)
+            }
         };
 
-        this.get = function () {
-            return send();
+        req.onerror = function (e) {
+            console.error(req.statusText);
         };
 
-        this.delete = function () {
-            return send();
-        };
+        req.send('{"a": "safasdf"}');
+        //req.send(null);
+        //req.send("fname=Henry&lname=Ford");
 
-        this.post = function () {
-            return send();
-        };
-
-        this.put = function () {
-            return send();
-        };
+        return deferred.promise;
     };
 
-    module.exports = new HttpClient();
-});
+    var client = {
+        getA: function () {
+            return send();
+        },
+        deleteA: function () {
+            return send();
+        },
+        postA: function () {
+            return send();
+        },
+        putA: function () {
+            return send();
+        }
+    };
+
+    module.exports = client;
+
+})();
